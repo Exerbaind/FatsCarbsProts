9da65de5bb95a5e5bfefce671e6caff8cbf6cb36
Exerbaind <digr98@gmail.com>
2021-03-15 12:55:43 +0300

    edit server port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 
-const PORT = config.get("port") || 5000;
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
