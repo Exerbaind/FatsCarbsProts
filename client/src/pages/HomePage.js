5b397cf6be68288d8b9f219e57ad7a3cd90ffc22
Exerbaind <digr98@gmail.com>
2021-03-19 10:59:28 +0300

    add normolize

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,7 +2,7 @@ const HomePage = () => {
   return (
     <div>
       {/* Возможно тут сделать статьи, которые связаны с питанием */}
-      <h1 className="page__title">Домашняя страница</h1>
+      <h1 className="page__title">Недавно добавленное</h1>
     </div>
   );
 };
