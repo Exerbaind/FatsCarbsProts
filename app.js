a73eb61a3666c73486690c53b1314f21b7173d00
Exerbaind <digr98@gmail.com>
2021-03-25 17:56:03 +0300

    images send and fetch TEST

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(async (req, res, next) => {
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./build/images");
+    cb(null, "./images");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
