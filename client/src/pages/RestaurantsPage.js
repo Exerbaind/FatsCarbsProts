9e644306036b33765c44392421df2bd60090c316
Exerbaind <digr98@gmail.com>
2021-04-09 12:19:34 +0300

    error fixes

--- client/src/pages/RestaurantsPage.js
+++ client/src/pages/RestaurantsPage.js
@@ -1,22 +1,12 @@
 import { useState } from "react";
-import foodData from "../api/restaurants";
 import SearchField from "../components/search/searchField";
 import RestaurantCard from "../components/restaurant-card/restaurantCard";
 const RestaurantsPage = () => {
   const [searchedRestourants, setSearchedRestourants] = useState(null);
-  const food = foodData();
-  const [restaurant, setRestaurant] = useState("");
 
   function searchAction(e) {
     e.preventDefault();
     let filtredArray = [];
-    for (let i = 0; i < food.length; i++) {
-      for (let j = 0; j < food[i].search_names.length; j++) {
-        if (food[i].search_names[j].includes(restaurant.toLocaleLowerCase())) {
-          filtredArray.push(food[i]);
-        }
-      }
-    }
     setSearchedRestourants(filtredArray);
   }
 
@@ -24,7 +14,6 @@ const RestaurantsPage = () => {
     <div className="page">
       <h1 className="page__title">Рестораны</h1>
       <SearchField
-        setRestaurant={setRestaurant}
         searchAction={searchAction}
         placeholder="Какой ресторан интересует?"
       />
--- client/src/pages/restaurant-page/RestaurantItemPage.js
+++ client/src/pages/restaurant-page/RestaurantItemPage.js
@@ -1,20 +1,8 @@
-import { useParams } from "react-router-dom";
-import foodData from "../../api/restaurants";
-import DishCard from "../../components/dish-card/dishCard";
 const RestaurantItemPage = () => {
-  const restaurantID = useParams().id;
-  const data = foodData();
-  const restaurant = data.filter((item) => +item.id === +restaurantID);
-
   return (
     <div className="restaurant-page">
-      <h1 className="restaurant-page__title">{restaurant[0].name}</h1>
       <h2 className="restaurant-page__list-title">Меню</h2>
-      <div className="restaurant-page__menu-list">
-        {restaurant[0].menu.map((dish) => {
-          return <DishCard dish={dish} key={dish.id} />;
-        })}
-      </div>
+      <div className="restaurant-page__menu-list"></div>
     </div>
   );
 };
